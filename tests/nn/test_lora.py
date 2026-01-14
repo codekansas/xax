f70e3728f3a358d413d7df510d0002d5bba226eb
Ben Bolte <ben@bolte.cc>
2026-01-13 16:57:40 -0800

    fix lora (#162)
    * fix lora
    
    * better defaults

--- tests/nn/test_lora.py
+++ tests/nn/test_lora.py
@@ -13,12 +13,12 @@ def test_lora_linear_matches_manual_delta() -> None:
     key = jrandom.key(0)
     lin_key, lora_key, x_key = jrandom.split(key, 3)
     linear = eqx.nn.Linear(4, 3, key=lin_key)
-    lora_linear = loraize_linear(linear, rank=2, alpha=2.0, key=lora_key)
+    lora_linear = loraize_linear(linear, rank=2, alpha=4.0, key=lora_key)  # alpha=4, rank=2 -> scaling=2.0
     x_bi = jrandom.normal(x_key, (2, 4))
 
     y_model_bo = lora_linear(x_bi, inference=True)
     manual_delta_bo = (x_bi @ lora_linear.lora_a_ir) @ lora_linear.lora_b_ro
-    expected_bo = jax.vmap(linear)(x_bi) + manual_delta_bo * lora_linear.alpha
+    expected_bo = jax.vmap(linear)(x_bi) + manual_delta_bo * lora_linear.scaling
 
     assert jnp.allclose(y_model_bo, expected_bo)
 
